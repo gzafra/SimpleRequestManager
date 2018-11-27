09952f850c258114069ca70c0873105271f3d40a
Guillermo Zafra <me@gzafra.com>
2018-11-27 15:24:04 +0100

    Imported project

---
+++ SimpleRequestManager/SimpleRequestManager/SimpleRequestManager.h
@@ -0,0 +1,19 @@
+//
+//  SimpleRequestManager.h
+//  SimpleRequestManager
+//
+//  Created by Guillermo Zafra on 27/11/2018.
+//  Copyright © 2018 Guillermo Zafra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SimpleRequestManager.
+FOUNDATION_EXPORT double SimpleRequestManagerVersionNumber;
+
+//! Project version string for SimpleRequestManager.
+FOUNDATION_EXPORT const unsigned char SimpleRequestManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleRequestManager/PublicHeader.h>
+
+
---
+++ SimpleRequestManager/SimpleRequestManager/Utilities/HTTPMethod.swift
@@ -0,0 +1,18 @@
+//
+//  HTTPMethod.swift
+//  SimpleRequestManager
+//
+//  Created by Guillermo Zafra on 27/11/2018.
+//  Copyright © 2018 Guillermo Zafra. All rights reserved.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get
+    case head
+    case post
+    case put
+    case patch
+    case delete
+}
